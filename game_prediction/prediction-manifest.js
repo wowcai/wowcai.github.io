fd8b72a34d97b12f8cdfcf8e10eb22aa8089d6cf
Chen_jm <Chen_jm@users.noreply.github.com>
2026-06-21 20:06:44 +0800

    update project

--- game_prediction/prediction-manifest.js
+++ game_prediction/prediction-manifest.js
@@ -103,21 +103,21 @@ window.WORLD_CUP_GAME_PREDICTION_MANIFEST = [
   {
     matchNo: "M37",
     matchFolder: "Belgium_vs_Iran_M37",
-    versions: ["2026.6.20.15.00"]
+    versions: ["2026.6.21.15.00"]
   },
   {
     matchNo: "M38",
     matchFolder: "New_Zealand_vs_Egypt_M38",
-    versions: ["2026.6.20.15.00"]
+    versions: ["2026.6.21.15.00"]
   },
   {
     matchNo: "M39",
     matchFolder: "Spain_vs_Saudi_Arabia_M39",
-    versions: ["2026.6.20.15.00"]
+    versions: ["2026.6.21.15.00"]
   },
   {
     matchNo: "M40",
     matchFolder: "Uruguay_vs_Cape_Verde_M40",
-    versions: ["2026.6.20.15.00"]
+    versions: ["2026.6.21.15.00"]
   }
 ];
